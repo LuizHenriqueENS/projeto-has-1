1195c7d4ad3f8fa7cce713e357af5d03db791d65
Luiz HenriqueENS <luizhenrique.egydio@gmail.com>
2023-07-15 14:57:19 -0300

    movimentacao basica do jogador adicionada

--- Assets/Scripts/PJ/PJ_Movimentacao.cs
+++ Assets/Scripts/PJ/PJ_Movimentacao.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PJ_Movimentacao : MonoBehaviour
 {
+
+    float movimentacaoHorizontal = 0f;
+    [SerializeField] float velocidadePJ = 30f;
+    Rigidbody2D rb;
+    [SerializeField] Joystick joystick;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        rb = FindObjectOfType<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(joystick.Horizontal >= .2f){
+            movimentacaoHorizontal = velocidadePJ;
+        } else if(joystick.Horizontal <= -.2f){
+            movimentacaoHorizontal = -velocidadePJ;
+        } else{
+            movimentacaoHorizontal = 0f;
+        }
+
+        rb.velocity = new Vector2(movimentacaoHorizontal * Time.deltaTime, rb.velocity.y);
     }
 }
